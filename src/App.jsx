cff9a7ff52b079d0bbbfd787c29512506bbbfd44
Sherzod <sherzodabdulhayev2802@gmail.com>
2024-12-20 17:45:50 +0900

    Update: Improve spacing and layout in mobile app solutions section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+// App.jsx - Fixed version
 import React from "react";
+import { ThemeProvider, useTheme } from "./context/ThemeContext";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -7,20 +9,30 @@ import About from "./pages/About";
 import Products from "./pages/Products";
 import Contact from "./pages/Contact";
 
+function AppContent() {
+  const { isDarkMode } = useTheme();
+
+  return (
+    <div className={`min-h-screen ${isDarkMode ? "dark" : ""}`}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<SampleWebsiteDesign />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
 function App() {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<SampleWebsiteDesign />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <ThemeProvider>
+      <Router>
+        <AppContent />
+      </Router>
+    </ThemeProvider>
   );
 }
 
